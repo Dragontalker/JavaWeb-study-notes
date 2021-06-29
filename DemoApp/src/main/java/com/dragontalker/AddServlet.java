8c8fbc07c282ffcc79536ae5966787ce82f886fa
Richard <richard.yang.tong@gmail.com>
2021-06-29 10:48:40 -0400

    Added notes.

--- DemoApp/src/main/java/com/dragontalker/AddServlet.java
+++ DemoApp/src/main/java/com/dragontalker/AddServlet.java
@@ -1,7 +1,12 @@
 package com.dragontalker;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class AddServlet extends HttpServlet{
+public class AddServlet extends HttpServlet {
 
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
 }
