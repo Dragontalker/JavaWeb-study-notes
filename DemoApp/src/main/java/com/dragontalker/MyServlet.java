c4018b3ba0e29689f726533fd595dc7bd3ce64e7
Richard <richard.yang.tong@gmail.com>
2021-06-29 12:51:28 -0400

    Added notes.

--- DemoApp/src/main/java/com/dragontalker/MyServlet.java
+++ DemoApp/src/main/java/com/dragontalker/MyServlet.java
@@ -3,6 +3,7 @@ package com.dragontalker;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +15,9 @@ public class MyServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.print("Hi<br>");
 		
+		ServletContext ctx = getServletContext();
+		String str = ctx.getInitParameter("name");
+		out.print(str);
 		
 	}
 
