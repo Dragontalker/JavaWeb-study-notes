8e40d70fca7e5fcaabb4754530340ea1a27a3cbe
Richard <richard.yang.tong@gmail.com>
2021-06-29 12:32:28 -0400

    Added notes.

--- DemoApp/src/main/java/com/dragontalker/AddServlet.java
+++ DemoApp/src/main/java/com/dragontalker/AddServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 
@@ -18,6 +19,9 @@ public class AddServlet extends HttpServlet {
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
 		
 //		req.setAttribute("k", k);
 //		
