f7c3a3b7ccb20db92617ebe5b6c0da75ee7c5b05
Richard <richard.yang.tong@gmail.com>
2021-06-29 10:57:45 -0400

    Added notes.

--- DemoApp/src/main/java/com/dragontalker/AddServlet.java
+++ DemoApp/src/main/java/com/dragontalker/AddServlet.java
@@ -1,12 +1,25 @@
 package com.dragontalker;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
 
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	private static final long serialVersionUID = -6200733901926457094L;
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("result is :" + k);
 	}
 }
