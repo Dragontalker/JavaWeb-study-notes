e893d25524544494ebb75227637b0ddd8b4ecdcc
Richard <richard.yang.tong@gmail.com>
2021-07-01 14:46:25 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/JdbcUtilsTest.java
+++ book/src/main/java/com/dragontalker/test/JdbcUtilsTest.java
@@ -1,11 +1,16 @@
 package com.dragontalker.test;
 
 import org.junit.Test;
+import com.dragontalker.utils.*;
 
 public class JdbcUtilsTest {
 	
 	@Test
 	public void testJdbcUltis() {
+		
+		for (int i = 0; i < 100; i++) {
+			System.out.println(">> " + (i + 1) + "th connection: " + JdbcUtils.getConnection());
+		}
 		
 	}
 
