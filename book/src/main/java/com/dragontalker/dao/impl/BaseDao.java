b467fcde7b7c8ffc13034bcbe3c82abade45bcf3
Richard <richard.yang.tong@gmail.com>
2021-07-01 14:56:31 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/dao/impl/BaseDao.java
+++ book/src/main/java/com/dragontalker/dao/impl/BaseDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 
 import com.dragontalker.utils.JdbcUtils;
 
@@ -19,9 +20,27 @@ public abstract class BaseDao {
 			return queryRunner.update(connection, sql, args);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			JdbcUtils.close(connection);
 		}
 		
 		return -1;
+		
+	}
+	
+	public <T> T  queryForOne(Class<T> type, String sql, Object ... args) {
+		
+		Connection connection = JdbcUtils.getConnection();
+		
+		try {
+			return queryRunner.query(connection, sql, new BeanHandler<T>(type), args);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.close(connection);
+		}
+		
+		return null;
 		
 	}
 	
