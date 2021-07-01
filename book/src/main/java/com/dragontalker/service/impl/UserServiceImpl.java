76ef3936ae6b180bb68f71d209651f01891e7929
Richard <richard.yang.tong@gmail.com>
2021-07-01 15:29:27 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/service/impl/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.dragontalker.service.impl;
+
+import com.dragontalker.pojo.User;
+import com.dragontalker.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void registerUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User login(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsUsername(String username) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
