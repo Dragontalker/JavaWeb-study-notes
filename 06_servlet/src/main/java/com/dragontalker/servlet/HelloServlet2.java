b317c079abebbfbe9feb489e0861bae33afcf092
Richard <richard.yang.tong@gmail.com>
2021-06-30 21:11:40 -0400

    Added notes.

---
+++ 06_servlet/src/main/java/com/dragontalker/servlet/HelloServlet2.java
@@ -0,0 +1,24 @@
+package com.dragontalker.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet2 extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
