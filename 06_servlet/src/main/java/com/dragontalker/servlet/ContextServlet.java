0354895382c3aafde2ac1d8951943c802b0d5544
Richard <richard.yang.tong@gmail.com>
2021-06-30 22:16:13 -0400

    Added notes.

---
+++ 06_servlet/src/main/java/com/dragontalker/servlet/ContextServlet.java
@@ -0,0 +1,8 @@
+package com.dragontalker.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class ContextServlet extends HttpServlet {
+	
+	
+}
--- 06_servlet/src/main/java/com/dragontalker/servlet/HelloServlet2.java
+++ 06_servlet/src/main/java/com/dragontalker/servlet/HelloServlet2.java
@@ -2,6 +2,7 @@ package com.dragontalker.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +13,18 @@ public class HelloServlet2 extends HttpServlet {
 	private static final long serialVersionUID = -1759121654483573069L;
 
 	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		System.out.println(">> Override init()...");
+	}
+
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doGet() method called from HelloServlet2");
+		
+		ServletConfig servletConfig = getServletConfig();
+		System.out.println(">> ServletConfig=" + servletConfig);
 	}
 
 	@Override
