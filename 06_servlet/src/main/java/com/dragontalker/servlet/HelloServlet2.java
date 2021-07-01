9ea59946ff3ce25f953a431f7a7a1f6c909db827
Richard <richard.yang.tong@gmail.com>
2021-06-30 21:14:36 -0400

    Added notes.

--- 06_servlet/src/main/java/com/dragontalker/servlet/HelloServlet2.java
+++ 06_servlet/src/main/java/com/dragontalker/servlet/HelloServlet2.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet2 extends HttpServlet {
 
+	private static final long serialVersionUID = -1759121654483573069L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
