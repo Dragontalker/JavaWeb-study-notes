abe80273c517a9cc07e1cb34cf88f2adf3a38ebf
Richard <richard.yang.tong@gmail.com>
2021-07-01 15:31:15 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/service/impl/UserServiceImpl.java
+++ book/src/main/java/com/dragontalker/service/impl/UserServiceImpl.java
@@ -1,20 +1,23 @@
 package com.dragontalker.service.impl;
 
+import com.dragontalker.dao.UserDao;
+import com.dragontalker.dao.impl.UserDaoImpl;
 import com.dragontalker.pojo.User;
 import com.dragontalker.service.UserService;
 
 public class UserServiceImpl implements UserService {
+	
+	private UserDao userDao = new UserDaoImpl();
 
 	@Override
 	public void registerUser(User user) {
-		// TODO Auto-generated method stub
+		userDao.saveUser(user);
 
 	}
 
 	@Override
 	public User login(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		return userDao.queryUserByUsernameAndPassword(user.getUsername(), user.getPassword());
 	}
 
 	@Override
