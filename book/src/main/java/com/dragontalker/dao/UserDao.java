40ea77575676c05343197f1db9b54fc120d8ae15
Richard <richard.yang.tong@gmail.com>
2021-07-01 15:11:30 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/dao/UserDao.java
+++ book/src/main/java/com/dragontalker/dao/UserDao.java
@@ -11,6 +11,16 @@ public interface UserDao {
 	 */
 	public User queryUserByUsername(String username);
 	
+	
+	/**
+	 * 根据用户名和密码查询用户信息
+	 * @param username
+	 * @param password
+	 * @return 如果返回null, 说明用户名和密码错误, 反之亦然
+	 */
+	public User queryUserByUsernameAndPassword(String username, String password);
+	
+	
 	/**
 	 * 保存用户信息
 	 * @param user 用户对象
