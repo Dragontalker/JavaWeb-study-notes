6c169b543d1672baec650b5021ba4053a607c7c0
Richard <richard.yang.tong@gmail.com>
2021-07-01 15:29:05 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/service/UserService.java
+++ book/src/main/java/com/dragontalker/service/UserService.java
@@ -1,5 +1,13 @@
 package com.dragontalker.service;
 
+import com.dragontalker.pojo.User;
+
 public interface UserService {
+	
+	public void registerUser(User user);
+	
+	public User login(User user);
+	
+	public boolean existsUsername(String username);
 
 }
