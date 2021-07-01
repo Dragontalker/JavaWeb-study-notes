ddad7eedd790031ac622af7a265f9061de2596a2
Richard <richard.yang.tong@gmail.com>
2021-07-01 15:09:30 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/dao/UserDao.java
+++ book/src/main/java/com/dragontalker/dao/UserDao.java
@@ -1,5 +1,14 @@
 package com.dragontalker.dao;
 
+import com.dragontalker.pojo.User;
+
 public interface UserDao {
 
+	/**
+	 * 根据用户名查询用户信息
+	 * @param username 用户名
+	 * @return 如果返回null, 说明没有这个用户, 反之亦然
+	 */
+	public User queryUserByUsername(String username);
+	
 }
