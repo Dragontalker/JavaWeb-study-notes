ce4d0c7da9dfebb29908ed589da998ebe5c34746
Richard <richard.yang.tong@gmail.com>
2021-07-01 18:00:51 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/LoginServlet.java
+++ book/src/main/java/com/dragontalker/web/LoginServlet.java
@@ -7,12 +7,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dragontalker.pojo.User;
+import com.dragontalker.service.UserService;
+import com.dragontalker.service.impl.UserServiceImpl;
+
 public class LoginServlet extends HttpServlet {
+	
+	private UserService userService = new UserServiceImpl();
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		
+		String username	= req.getParameter("username");
+		String password	= req.getParameter("password");
+		
+		if (userService.existsUsername(username)) {
+			
+			if (userService.login(new User(null, username, password, null)) != null ) {
+				System.out.println("登录成功!");
+				req.getRequestDispatcher("/pages/user/login_success.html").forward(req, resp);
+			} else {
+				System.out.println("密码错误!");
+				req.getRequestDispatcher("/pages/user/login.html").forward(req, resp);
+			}
+			
+		} else {
+			System.out.println("用户名[" + username +"]不存在!");
+			req.getRequestDispatcher("/pages/user/login.html").forward(req, resp);
+		}
 	}
 
 }
