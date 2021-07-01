5485e0b4a900b96c7930b7e88b4e9901977e941d
Richard <richard.yang.tong@gmail.com>
2021-07-01 10:00:41 -0400

    Added notes.

--- 07_servlet/src/main/java/com/dragontalker/servlet/Servlet1.java
+++ 07_servlet/src/main/java/com/dragontalker/servlet/Servlet1.java
@@ -2,6 +2,7 @@ package com.dragontalker.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +17,10 @@ public class Servlet1 extends HttpServlet {
 		System.out.println("@Servlet1 => check data... => username=" + username);
 		
 		req.setAttribute("key", "Servlet1 Approved");
+		
+		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/servlet2");
+		
+		requestDispatcher.forward(req, resp);
 	}
 
 }
