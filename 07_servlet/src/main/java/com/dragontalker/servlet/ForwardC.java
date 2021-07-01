1b77a70cd29297473fcdf85920e18bbc800b4edf
Richard <richard.yang.tong@gmail.com>
2021-07-01 10:40:06 -0400

    Added notes.

--- 07_servlet/src/main/java/com/dragontalker/servlet/ForwardC.java
+++ 07_servlet/src/main/java/com/dragontalker/servlet/ForwardC.java
@@ -1,7 +1,20 @@
 package com.dragontalker.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ForwardC extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		System.out.println("@ForwardC Servlet...");
+		
+		req.getRequestDispatcher("/a/b/c.html").forward(req, resp);
+	}
 
 }
