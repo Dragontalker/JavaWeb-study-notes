0e50353c6549dd677895c5ea9c089dd47b0ad423
Richard <richard.yang.tong@gmail.com>
2021-07-01 12:12:03 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/User.java
+++ book/src/main/java/com/dragontalker/pojo/User.java
@@ -7,4 +7,13 @@ public class User {
 	private String password;
 	private String email;
 
+	public User() {}
+
+	public User(Integer idInteger, String username, String password, String email) {
+		this.idInteger = idInteger;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	
 }
