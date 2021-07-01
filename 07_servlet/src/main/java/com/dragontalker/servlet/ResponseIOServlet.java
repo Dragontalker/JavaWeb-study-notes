9c40ae48dc88aef1e02643136e3017152366774f
Richard <richard.yang.tong@gmail.com>
2021-07-01 11:12:47 -0400

    Added notes.

--- 07_servlet/src/main/java/com/dragontalker/servlet/ResponseIOServlet.java
+++ 07_servlet/src/main/java/com/dragontalker/servlet/ResponseIOServlet.java
@@ -1,6 +1,7 @@
 package com.dragontalker.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +13,10 @@ public class ResponseIOServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		resp.getWriter();
-		resp.getOutputStream();
+		PrintWriter writer = resp.getWriter();
+		writer.write(">> Response content!!");
+		
+		
 	}
 
 }
