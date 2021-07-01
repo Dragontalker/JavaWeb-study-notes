8325dc1a001d03240a0658b2a6c97c33061f26c2
Richard <richard.yang.tong@gmail.com>
2021-07-01 15:17:32 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/UserDaoTest.java
+++ book/src/main/java/com/dragontalker/test/UserDaoTest.java
@@ -2,10 +2,14 @@ package com.dragontalker.test;
 
 import org.junit.Test;
 
+import com.dragontalker.dao.UserDao;
+import com.dragontalker.dao.impl.UserDaoImpl;
+
 public class UserDaoTest {
 
 	@Test
 	public void testQueryUserByUsername() {
-		
+		UserDao userDao = new UserDaoImpl();
+		System.out.println(userDao.queryUserByUsername("admin"));
 	}
 }
