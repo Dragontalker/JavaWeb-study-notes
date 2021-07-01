ad8c1c9e67cf52efbc18558a8118aac4eac64d48
Richard <richard.yang.tong@gmail.com>
2021-06-30 22:17:39 -0400

    Added notes.

--- 06_servlet/src/main/java/com/dragontalker/servlet/ContextServlet.java
+++ 06_servlet/src/main/java/com/dragontalker/servlet/ContextServlet.java
@@ -1,8 +1,21 @@
 package com.dragontalker.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ContextServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 277148871386430658L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
 	
 	
 }
