cc7cc093f01716ff5cc2a1473c1cba41064625c4
Richard <richard.yang.tong@gmail.com>
2021-07-01 09:17:50 -0400

    Added notes.

--- 07_servlet/src/main/java/com/dragontalker/servlet/RequestAPIServlet.java
+++ 07_servlet/src/main/java/com/dragontalker/servlet/RequestAPIServlet.java
@@ -11,8 +11,10 @@ public class RequestAPIServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
+		System.out.println("URI => " + req.getRequestURI());
+		
+		System.out.println("URL => " + req.getRequestURL());
 	}
 
 	
