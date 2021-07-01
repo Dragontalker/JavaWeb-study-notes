578bf8899fcd11d04d12d7526c353596bfdd0355
Richard <richard.yang.tong@gmail.com>
2021-07-01 09:58:01 -0400

    Added notes.

--- 07_servlet/src/main/java/com/dragontalker/servlet/Servlet1.java
+++ 07_servlet/src/main/java/com/dragontalker/servlet/Servlet1.java
@@ -13,7 +13,9 @@ public class Servlet1 extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		String username = req.getParameter("username");
-		System.out.println("@Servlet1 => check data => username=" + username);
+		System.out.println("@Servlet1 => check data... => username=" + username);
+		
+		req.setAttribute("key", "Servlet1 Approved");
 	}
 
 }
