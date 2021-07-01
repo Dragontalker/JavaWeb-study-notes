37d4d948382e9cf94eb67fefc34d5f14cc5f845e
Richard <richard.yang.tong@gmail.com>
2021-07-01 15:11:55 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/dao/impl/UserDaoImpl.java
@@ -0,0 +1,26 @@
+package com.dragontalker.dao.impl;
+
+import com.dragontalker.dao.UserDao;
+import com.dragontalker.pojo.User;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public User queryUserByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User queryUserByUsernameAndPassword(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int saveUser(User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
