5ab1e0489add69dfc842bac7610fef0ca4e99bb8
Richard <richard.yang.tong@gmail.com>
2021-07-01 12:08:39 -0400

    Added notes.

---
+++ book/bookDB.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS book;
+
+CREATE DATABASE book;
+
+USE book;
+
+CREATE TABLE t_user(
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(20) NOT NULL UNIQUE,
+    `password` VARCHAR(32) NOT NULL,
+    `email` VARCHAR(200)
+);
