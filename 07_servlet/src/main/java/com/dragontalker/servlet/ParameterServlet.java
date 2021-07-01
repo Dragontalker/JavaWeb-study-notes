5001e46ff024b89757ffe17d142ed5da15896995
Richard <richard.yang.tong@gmail.com>
2021-07-01 09:33:58 -0400

    Added notes.

--- 07_servlet/src/main/java/com/dragontalker/servlet/ParameterServlet.java
+++ 07_servlet/src/main/java/com/dragontalker/servlet/ParameterServlet.java
@@ -11,8 +11,14 @@ public class ParameterServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
+		String userName = req.getParameter("username");
+		String password = req.getParameter("password");
+		String hobby = req.getParameter("hobby");
+		
+		System.out.println(">> User Name=" + userName);
+		System.out.println(">> Password=" + password);
+		System.out.println(">> Hobby=" + hobby);
 	}
 
 	
