d9ad206551e3ab0ef745646a679b57758f6b5507
Richard <richard.yang.tong@gmail.com>
2021-07-01 16:27:30 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/RegisterServlet.java
+++ book/src/main/java/com/dragontalker/web/RegisterServlet.java
@@ -7,7 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dragontalker.pojo.User;
+import com.dragontalker.service.UserService;
+import com.dragontalker.service.impl.UserServiceImpl;
+
 public class RegisterServlet extends HttpServlet {
+	
+	private UserService userService = new UserServiceImpl();
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -19,9 +25,17 @@ public class RegisterServlet extends HttpServlet {
 		
 		if ("abcde".equalsIgnoreCase(code)) {
 			
+			if (userService.existsUsername(username)) {
+				System.out.println("用户名[" + username +"]已存在!");
+				req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
+			} else {
+				userService.registerUser(new User(null, username, password, email));
+				req.getRequestDispatcher("/pages/user/regist_success.html").forward(req, resp);
+			}
+			
 		} else {
 			System.out.println("验证码[" + code + "]错误!");
-			req.getRequestDispatcher("/pages/user/register.html").forward(req, resp);
+			req.getRequestDispatcher("/pages/user/regist.html").forward(req, resp);
 		}
 	}
 	
