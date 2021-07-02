a40c793fde52ad0da443285cf5aae2e8d8dd1b59
Richard <richard.yang.tong@gmail.com>
2021-07-02 17:18:03 -0400

    Added notes.

--- 09_EL_JSTL/src/main/java/com/dragontalker/pojo/Student.java
+++ 09_EL_JSTL/src/main/java/com/dragontalker/pojo/Student.java
@@ -17,5 +17,45 @@ public class Student {
 		this.age = age;
 		this.phone = phone;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 	
 }
