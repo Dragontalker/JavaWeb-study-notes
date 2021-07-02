29c6b88cbbc624632a2d9b4c27b1b971070cf185
Richard <richard.yang.tong@gmail.com>
2021-07-02 11:38:02 -0400

    Added notes.

---
+++ 08_jsp/src/main/java/com/dragontalker/listener/MyServletContextListener.java
@@ -0,0 +1,21 @@
+package com.dragontalker.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyServletContextListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		ServletContextListener.super.contextDestroyed(sce);
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		ServletContextListener.super.contextInitialized(sce);
+	}
+
+	
+}
