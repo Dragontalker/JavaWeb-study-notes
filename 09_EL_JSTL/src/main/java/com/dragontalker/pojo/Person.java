3cd94c4cd6d86bbd3a6eba9982360465237f532f
Richard <richard.yang.tong@gmail.com>
2021-07-02 13:25:29 -0400

    Added notes.

--- 09_EL_JSTL/src/main/java/com/dragontalker/pojo/Person.java
+++ 09_EL_JSTL/src/main/java/com/dragontalker/pojo/Person.java
@@ -1,5 +1,14 @@
 package com.dragontalker.pojo;
 
+import java.util.List;
+import java.util.Map;
+
 public class Person {
 
+	private String name;
+	private String[] phone;
+	private List<String> cities;
+	private Map<String, String> map;
+	
+	
 }
