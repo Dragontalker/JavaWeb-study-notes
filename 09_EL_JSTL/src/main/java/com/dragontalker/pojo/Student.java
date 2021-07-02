f78457ef0594d2c4b723d84022fc86b81d12e877
Richard <richard.yang.tong@gmail.com>
2021-07-02 17:17:32 -0400

    Added notes.

--- 09_EL_JSTL/src/main/java/com/dragontalker/pojo/Student.java
+++ 09_EL_JSTL/src/main/java/com/dragontalker/pojo/Student.java
@@ -1,5 +1,11 @@
 package com.dragontalker.pojo;
 
 public class Student {
+	
+	private Integer id;
+	private String username;
+	private String password;
+	private Integer age;
+	private String phone;
 
 }
