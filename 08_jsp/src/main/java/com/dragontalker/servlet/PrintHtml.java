24977f355713178b84e738bdbf74f48a8c6533a2
Richard <richard.yang.tong@gmail.com>
2021-07-01 20:36:24 -0400

    Added notes.

--- 08_jsp/src/main/java/com/dragontalker/servlet/PrintHtml.java
+++ 08_jsp/src/main/java/com/dragontalker/servlet/PrintHtml.java
@@ -13,16 +13,18 @@ public class PrintHtml extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		resp.setContentType("text/html; charset=UTF-8");
+		
 		PrintWriter writer = resp.getWriter();
 		
 		writer.write("<!DOCTYPE html>\r\n");
 		writer.write("<html>\r\n");
 		writer.write("<head>\r\n");
-		writer.write("<meta charset=\"ISO-8859-1\">\r\n");
+		writer.write("<meta charset=\"UTF-8\">\r\n");
 		writer.write("<title>Insert title here</title>\r\n");
 		writer.write("</head>\r\n");
 		writer.write("<body>\r\n");
-		writer.write("Hello world!\r\n");
+		writer.write("这里是html页面\r\n");
 		writer.write("</body>\r\n");
 		writer.write("</html>\r\n");
 	}
