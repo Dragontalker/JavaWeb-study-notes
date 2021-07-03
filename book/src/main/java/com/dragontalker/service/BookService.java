04163f9df6f2026bcf54776b68b066028c80d18f
Richard <richard.yang.tong@gmail.com>
2021-07-03 17:13:34 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/service/BookService.java
+++ book/src/main/java/com/dragontalker/service/BookService.java
@@ -1,5 +1,10 @@
 package com.dragontalker.service;
 
+import com.dragontalker.pojo.Book;
+
 public interface BookService {
 
+	public void addBook(Book book);
+	
+	public void deleteBook(Book book);
 }
