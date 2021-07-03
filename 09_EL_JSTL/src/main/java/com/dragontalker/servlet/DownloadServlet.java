dc16452a2611933c195746ff31e42bd76decd616
Richard <richard.yang.tong@gmail.com>
2021-07-03 06:01:14 -0400

    Added notes.

--- 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
+++ 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
@@ -1,12 +1,16 @@
 package com.dragontalker.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 
 public class DownloadServlet extends HttpServlet {
 
@@ -18,10 +22,13 @@ public class DownloadServlet extends HttpServlet {
 		
 		// 2. 读取要下载的文件内容(通过ServletContext对象可以读取)
 		ServletContext servletContext = getServletContext();
-		/**
-		 * /斜杆被服务器解析表示地址为http://ip:port/工程名/ 映射 到代码的web目录
-		 */
-		servletContext.getResourceAsStream("/file/");
+		
+		// 斜杆被服务器解析表示地址为http://ip:port/工程名/ 映射 到代码的web目录
+		InputStream resourceAStream = servletContext.getResourceAsStream("/file/");
+		
+		// 获取响应的输出流
+		OutputStream outputStream = resp.getOutputStream();
+		
 	}
 	
 	
