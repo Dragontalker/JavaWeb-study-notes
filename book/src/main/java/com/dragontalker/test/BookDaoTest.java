64da50480ca1ae5781fedcc93b4f872ddbaa9bf6
Richard <richard.yang.tong@gmail.com>
2021-07-03 17:00:50 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/BookDaoTest.java
+++ book/src/main/java/com/dragontalker/test/BookDaoTest.java
@@ -1,5 +1,10 @@
 package com.dragontalker.test;
 
+import com.dragontalker.dao.BookDao;
+import com.dragontalker.dao.impl.BookDaoImpl;
+
 public class BookDaoTest {
+	
+	private BookDao bookdao = new BookDaoImpl();
 
 }
