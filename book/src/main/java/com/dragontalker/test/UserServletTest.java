299fc731fa8f681dafeec371d4b9893130399689
Richard <richard.yang.tong@gmail.com>
2021-07-03 10:02:50 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/test/UserServletTest.java
@@ -0,0 +1,18 @@
+package com.dragontalker.test;
+
+public class UserServletTest {
+	
+	public void login() {
+		System.out.println(">> login() called...");
+	}
+	
+	public void regist() {
+		System.out.println(">> regist() called...");
+	}
+	
+	public void updateUser() {
+		System.out.println(">> updateUser() called...");
+	}
+	
+
+}
