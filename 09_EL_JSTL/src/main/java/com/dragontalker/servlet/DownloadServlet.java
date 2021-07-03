ab883484e3d7abba15ebe6457f5ae895068420c3
Richard <richard.yang.tong@gmail.com>
2021-07-03 06:06:19 -0400

    Added notes.

--- 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
+++ 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
@@ -23,12 +23,20 @@ public class DownloadServlet extends HttpServlet {
 		// 2. 读取要下载的文件内容(通过ServletContext对象可以读取)
 		ServletContext servletContext = getServletContext();
 		
+		// 获取要下载的文件类型
+		String mimeType = servletContext.getMimeType("/file/" + downloadFileName);
+		System.out.println("下载的文件类型: " + mimeType);
+		
+		// 4. 在回传前, 通过响应头告诉客户端返回的数据类型
+		resp.setContentType(downloadFileName);
+		
 		// 斜杆被服务器解析表示地址为http://ip:port/工程名/ 映射 到代码的web目录
-		InputStream resourceAStream = servletContext.getResourceAsStream("/file/");
+		InputStream resourceAStream = servletContext.getResourceAsStream("/file/" + downloadFileName);
 		
 		// 获取响应的输出流
 		OutputStream outputStream = resp.getOutputStream();
 		
+		// 3. 把下载的文件内容回传给客户端
 		// 读取输入流中全部的数据, 输出给输出流, 输出给客户端
 		IOUtils.copy(resourceAStream, outputStream);
 		
