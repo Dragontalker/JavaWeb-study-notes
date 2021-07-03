cb001ab82d3927db138d923a28d746041913b67d
Richard <richard.yang.tong@gmail.com>
2021-07-03 18:59:36 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/BookServlet.java
+++ book/src/main/java/com/dragontalker/web/BookServlet.java
@@ -1,11 +1,13 @@
 package com.dragontalker.web;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dragontalker.pojo.Book;
 import com.dragontalker.service.BookService;
 import com.dragontalker.service.impl.BookServiceImpl;
 
@@ -27,10 +29,13 @@ public class BookServlet extends BaseServlet {
 	
 	protected void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 1. 通过BookService查询全部图书
+		List<Book> books = bookService.queryBooks();
 		
 		// 2. 把全部图书保存到Request域中
+		req.setAttribute("books", books);
 		
 		// 3. 请求妆发到/pages/manager/book_manager.jsp页面
+		req.getRequestDispatcher("/pages/manager/book_manager.jsp").forward(req, resp);
 	}
 
 }
