b3e20684aa17db4dabffb1c8c999c72630ec3417
Richard <richard.yang.tong@gmail.com>
2021-07-03 17:02:58 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/BookDaoTest.java
+++ book/src/main/java/com/dragontalker/test/BookDaoTest.java
@@ -1,10 +1,21 @@
 package com.dragontalker.test;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
 import com.dragontalker.dao.BookDao;
 import com.dragontalker.dao.impl.BookDaoImpl;
+import com.dragontalker.pojo.Book;
 
 public class BookDaoTest {
 	
 	private BookDao bookdao = new BookDaoImpl();
+	
+	@Test
+	public void testAddBook() {
+		Book book = new Book(null, "国哥为什么这么帅!", "191125", new BigDecimal(9999), 110000, 0, null);
+		bookdao.addBook(book);
+	}
 
 }
