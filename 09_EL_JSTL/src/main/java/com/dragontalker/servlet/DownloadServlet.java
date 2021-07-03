d97a37ab1db76c6e541bfef7495493dba1dd40fd
Richard <richard.yang.tong@gmail.com>
2021-07-03 05:56:11 -0400

    Added notes.

--- 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
+++ 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
@@ -11,8 +11,9 @@ public class DownloadServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
+		// 1. 获取要下载的文件名
+		String downloadFileName = "2.jpg";
 	}
 	
 	
