11e782303f714aed3679cad888b480fb7803ac06
Richard <richard.yang.tong@gmail.com>
2021-07-03 10:59:16 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/utils/WebUltis.java
+++ book/src/main/java/com/dragontalker/utils/WebUltis.java
@@ -1,5 +1,15 @@
 package com.dragontalker.utils;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class WebUltis {
 
+	public static void copyParamToBean(HttpServletRequest request, Object bean) {
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
