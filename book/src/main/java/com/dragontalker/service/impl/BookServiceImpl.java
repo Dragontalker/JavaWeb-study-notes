548af60395312fb76f9b642a4ad0dda494f46608
Richard <richard.yang.tong@gmail.com>
2021-07-03 17:14:42 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/service/impl/BookServiceImpl.java
@@ -0,0 +1,40 @@
+package com.dragontalker.service.impl;
+
+import java.util.List;
+
+import com.dragontalker.pojo.Book;
+import com.dragontalker.service.BookService;
+
+public class BookServiceImpl implements BookService {
+
+	@Override
+	public void addBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateBook(Book book) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Book queryBookById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> queryBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
