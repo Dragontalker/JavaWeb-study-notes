7dc197f5f4445fa5edfb69aa6fd8fd6a86ca98c9
Richard <richard.yang.tong@gmail.com>
2021-07-03 06:10:37 -0400

    Added notes.

--- 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
+++ 09_EL_JSTL/src/main/java/com/dragontalker/servlet/DownloadServlet.java
@@ -30,6 +30,11 @@ public class DownloadServlet extends HttpServlet {
 		// 4. 在回传前, 通过响应头告诉客户端返回的数据类型
 		resp.setContentType(downloadFileName);
 		
+		// 5. 还要告诉客户端收到的数据是用于下载的(还是使用响应头)
+		// Content-Disposition响应头, 表示收到的数据怎么处理
+		// attachment表示附件, 表示下载使用
+		resp.setHeader("Context-Disposition", "attachment; filename=" + downloadFileName);
+		
 		// 斜杆被服务器解析表示地址为http://ip:port/工程名/ 映射 到代码的web目录
 		InputStream resourceAStream = servletContext.getResourceAsStream("/file/" + downloadFileName);
 		
