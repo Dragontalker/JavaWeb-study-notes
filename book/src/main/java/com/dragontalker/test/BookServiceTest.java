b86ec01e9200fc6abd6b06295537bf22b605abf5
Richard <richard.yang.tong@gmail.com>
2021-07-03 17:28:02 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/BookServiceTest.java
+++ book/src/main/java/com/dragontalker/test/BookServiceTest.java
@@ -1,5 +1,19 @@
 package com.dragontalker.test;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.dragontalker.pojo.Book;
+import com.dragontalker.service.BookService;
+import com.dragontalker.service.impl.BookServiceImpl;
+
 public class BookServiceTest {
 
+	private BookService bookService = new BookServiceImpl();
+	
+	@Test
+	public void testAddBook() {
+		bookService.addBook(new Book(null, "GuoGeZaiShou, TianXiaWoYou", "1125", new BigDecimal(100000), 11125, 0, null));
+	}
 }
