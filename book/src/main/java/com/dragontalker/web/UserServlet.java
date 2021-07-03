f20a237b3d9e11d3a8bf412f45a63098fa42c9fc
Richard <richard.yang.tong@gmail.com>
2021-07-03 11:03:55 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/UserServlet.java
+++ book/src/main/java/com/dragontalker/web/UserServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.dragontalker.pojo.User;
 import com.dragontalker.service.UserService;
 import com.dragontalker.service.impl.UserServiceImpl;
+import com.dragontalker.utils.WebUltis;
 
 public class UserServlet extends BaseServlet {
 	
@@ -34,27 +35,27 @@ public class UserServlet extends BaseServlet {
 	
 	protected void regist(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		String username	= req.getParameter("username");
-		String password	= req.getParameter("password");
-		String email = req.getParameter("email");
+		User user = new User();
+		WebUltis.copyParamToBean(req, user);
+		
 		String code	= req.getParameter("code");
 		
 		if ("abcde".equalsIgnoreCase(code)) {
 			
-			if (userService.existsUsername(username)) {
+			if (userService.existsUsername(user.getUsername())) {
 				req.setAttribute("msg", "用户名已存在!");
-				req.setAttribute("username", username);
-				req.setAttribute("email", email);
+				req.setAttribute("username", user.getUsername());
+				req.setAttribute("email", user.getEmail());
 				req.getRequestDispatcher("/pages/user/regist.jsp").forward(req, resp);
 			} else {
-				userService.registerUser(new User(null, username, password, email));
+				userService.registerUser(new User(null, user.getUsername(), user.getPassword(), user.getEmail()));
 				req.getRequestDispatcher("/pages/user/regist_success.jsp").forward(req, resp);
 			}
 			
 		} else {
 			req.setAttribute("msg", "验证码错误!");
-			req.setAttribute("username", username);
-			req.setAttribute("email", email);
+			req.setAttribute("username", user.getUsername());
+			req.setAttribute("email", user.getEmail());
 			req.getRequestDispatcher("/pages/user/regist.jsp").forward(req, resp);
 		}
 	}
