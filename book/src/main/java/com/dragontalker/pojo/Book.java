b361cfc91f37940e93802eddaad53a2a7bd14cf4
Richard <richard.yang.tong@gmail.com>
2021-07-03 16:42:47 -0400

    Added notes.[A

--- book/src/main/java/com/dragontalker/pojo/Book.java
+++ book/src/main/java/com/dragontalker/pojo/Book.java
@@ -25,5 +25,61 @@ public class Book {
 		this.stock = stock;
 		this.imgPath = imgPath;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Integer getSales() {
+		return sales;
+	}
+
+	public void setSales(Integer sales) {
+		this.sales = sales;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public String getImgPath() {
+		return imgPath;
+	}
+
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
 	
 }
