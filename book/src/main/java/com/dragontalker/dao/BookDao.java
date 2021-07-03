a7161da76e18081caa3eeecea9e43127d2a4d2e1
Richard <richard.yang.tong@gmail.com>
2021-07-03 16:50:44 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/dao/BookDao.java
+++ book/src/main/java/com/dragontalker/dao/BookDao.java
@@ -1,5 +1,7 @@
 package com.dragontalker.dao;
 
+import java.util.List;
+
 import com.dragontalker.pojo.Book;
 
 public interface BookDao {
@@ -7,4 +9,10 @@ public interface BookDao {
 	public int addBook(Book book);
 	
 	public int deleteBookById(Integer id);
+	
+	public int updateBook(Book book);
+	
+	public Book queryBookById(Integer id);
+	
+	public List<Book> queryBooks();
 }
