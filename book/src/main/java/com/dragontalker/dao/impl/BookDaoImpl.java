093644425bd05a195f66a959c082b9336522e294
Richard <richard.yang.tong@gmail.com>
2021-07-03 16:51:11 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/dao/impl/BookDaoImpl.java
@@ -0,0 +1,40 @@
+package com.dragontalker.dao.impl;
+
+import java.util.List;
+
+import com.dragontalker.dao.BookDao;
+import com.dragontalker.pojo.Book;
+
+public class BookDaoImpl extends BaseDao implements BookDao {
+
+	@Override
+	public int addBook(Book book) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteBookById(Integer id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateBook(Book book) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Book queryBookById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> queryBooks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
