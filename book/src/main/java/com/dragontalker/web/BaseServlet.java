ec003d243243b2f037208cdb96969efbe3a3282d
Richard <richard.yang.tong@gmail.com>
2021-07-03 10:23:37 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/BaseServlet.java
+++ book/src/main/java/com/dragontalker/web/BaseServlet.java
@@ -1,7 +1,31 @@
 package com.dragontalker.web;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public abstract class BaseServlet extends HttpServlet {
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String action = req.getParameter("action");
+		
+		try {
+			Method method = this.getClass()
+					.getDeclaredMethod(action, 
+							HttpServletRequest.class, 
+							HttpServletResponse.class);
+			
+			method.invoke(this, req, resp);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
 }
--- book/src/main/java/com/dragontalker/web/UserServlet.java
+++ book/src/main/java/com/dragontalker/web/UserServlet.java
@@ -16,23 +16,7 @@ public class UserServlet extends HttpServlet {
 	
 	private UserService userService = new UserServiceImpl();
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String action = req.getParameter("action");
-		
-		try {
-			Method method = this.getClass()
-					.getDeclaredMethod(action, 
-							HttpServletRequest.class, 
-							HttpServletResponse.class);
-			
-			method.invoke(this, req, resp);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-	}
+	
 
 	protected void login(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
