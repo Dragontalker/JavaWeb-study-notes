ac9a79255034d5b98d5ee6bdc4895b34ec8739e4
Richard <richard.yang.tong@gmail.com>
2021-07-03 16:41:34 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/Book.java
+++ book/src/main/java/com/dragontalker/pojo/Book.java
@@ -1,5 +1,15 @@
 package com.dragontalker.pojo;
 
+import java.math.BigDecimal;
+
 public class Book {
+	
+	private Integer id;
+	private String name;
+	private String author;
+	private BigDecimal price;
+	private Integer sales;
+	private Integer stock;
+	private String imgPath = "";
 
 }
