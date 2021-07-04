dbefc1e87f351f16da404910ed3bf0c1609679cf
Richard <richard.yang.tong@gmail.com>
2021-07-04 06:11:12 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/Page.java
+++ book/src/main/java/com/dragontalker/pojo/Page.java
@@ -1,5 +1,7 @@
 package com.dragontalker.pojo;
 
+import java.util.List;
+
 public class Page {
 	
 	public static final Integer PAGE_SIZE = 4;
@@ -7,4 +9,6 @@ public class Page {
 	private Integer pageNo;
 	private Integer pageTotal;
 	private Integer pageSize = PAGE_SIZE;
+	private Integer pageTotalCount;
+	private List<Book> books;
 }
