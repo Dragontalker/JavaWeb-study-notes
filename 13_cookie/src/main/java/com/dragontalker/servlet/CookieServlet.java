4a92a51d88b03e3e498579d2c3bca5eb52c6f2e5
Richard <richard.yang.tong@gmail.com>
2021-07-04 15:27:21 -0400

    Added notes.

--- 13_cookie/src/main/java/com/dragontalker/servlet/CookieServlet.java
+++ 13_cookie/src/main/java/com/dragontalker/servlet/CookieServlet.java
@@ -1,7 +1,17 @@
 package com.dragontalker.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class CookieServlet extends BaseServlet {
 
 	private static final long serialVersionUID = 3530097617786374562L;
+	
+    protected void createCookie(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	
+    }
 
 }
