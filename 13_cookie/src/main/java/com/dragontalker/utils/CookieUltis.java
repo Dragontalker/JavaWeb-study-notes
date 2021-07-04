8c2b8cebc4efec7d7da51ae1b91ba764211de54d
Richard <richard.yang.tong@gmail.com>
2021-07-04 15:59:33 -0400

    Added notes.

--- 13_cookie/src/main/java/com/dragontalker/utils/CookieUltis.java
+++ 13_cookie/src/main/java/com/dragontalker/utils/CookieUltis.java
@@ -6,6 +6,16 @@ public class CookieUltis {
 
 	public static Cookie findCookie(String name, Cookie[] cookies) {
 		
+		if (name == null || cookies == null || cookies.length == 0) {
+			return null;
+		}
+		
+		for (Cookie cookie : cookies) {
+			if (name.equals(cookie.getName())) {
+				return cookie;
+			}
+		}
+		
 		return null;
 	}
 }
