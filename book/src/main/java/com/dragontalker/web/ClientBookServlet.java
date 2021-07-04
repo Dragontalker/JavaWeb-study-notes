743b01eb900fb8ba6c336a027e74274554e32a96
Richard <richard.yang.tong@gmail.com>
2021-07-04 11:50:29 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/ClientBookServlet.java
+++ book/src/main/java/com/dragontalker/web/ClientBookServlet.java
@@ -1,5 +1,37 @@
 package com.dragontalker.web;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dragontalker.pojo.Book;
+import com.dragontalker.pojo.Page;
+import com.dragontalker.service.BookService;
+import com.dragontalker.service.impl.BookServiceImpl;
+import com.dragontalker.utils.WebUltis;
+
 public class ClientBookServlet extends BaseServlet {
 
+	private BookService bookService = new BookServiceImpl();
+	
+	protected void page(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		System.out.println("经过了前台的ClientBookServlet程序");
+		
+		// 1. 获取请求的参数 pageNo 和 pageSize
+		int pageNo = WebUltis.parseInt(req.getParameter("pageNo"), 1);
+		int pageSize = WebUltis.parseInt(req.getParameter("pageSize"), Page.PAGE_SIZE);
+		
+		// 2. 调用BookService.page(pageNo, pageSize): Page对象
+		Page<Book> page = bookService.page(pageNo, pageSize);
+		
+		// 3. 保存Page对象到Request域中
+		req.setAttribute("page", page);
+		
+		// 4. 请求转发到pages/manager/book_manager.jsp
+		req.getRequestDispatcher("/pages/client/index.jsp").forward(req, resp);
+	}
+	
 }
