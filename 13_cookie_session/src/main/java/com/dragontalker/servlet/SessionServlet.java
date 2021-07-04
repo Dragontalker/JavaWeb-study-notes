c321dbafeebc2ae4ee390f489ef96d182aef4a42
Richard <richard.yang.tong@gmail.com>
2021-07-04 17:38:58 -0400

    Added notes.

--- 13_cookie_session/src/main/java/com/dragontalker/servlet/SessionServlet.java
+++ 13_cookie_session/src/main/java/com/dragontalker/servlet/SessionServlet.java
@@ -1,5 +1,21 @@
 package com.dragontalker.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 public class SessionServlet extends BaseServlet {
+
+	protected void createOrGetSession(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		
+		boolean isNew = session.isNew();
+		
+		String id = session.getId();
+	}
 
 }
