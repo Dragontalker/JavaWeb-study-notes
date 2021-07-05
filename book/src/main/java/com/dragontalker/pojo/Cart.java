3419a30e308365e669fbb50c4ccc860ac70c3562
Richard <richard.yang.tong@gmail.com>
2021-07-05 11:15:08 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/Cart.java
+++ book/src/main/java/com/dragontalker/pojo/Cart.java
@@ -9,23 +9,34 @@ public class Cart {
 	private Integer totalCount;
 	private BigDecimal totalPrice;
 	private List<CartItem> items = new ArrayList<CartItem>();
+	
 	public Integer getTotalCount() {
 		return totalCount;
 	}
+	
 	public void setTotalCount(Integer totalCount) {
 		this.totalCount = totalCount;
 	}
+	
 	public BigDecimal getTotalPrice() {
 		return totalPrice;
 	}
+	
 	public void setTotalPrice(BigDecimal totalPrice) {
 		this.totalPrice = totalPrice;
 	}
+	
 	public List<CartItem> getItems() {
 		return items;
 	}
+	
 	public void setItems(List<CartItem> items) {
 		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart [totalCount=" + totalCount + ", totalPrice=" + totalPrice + ", items=" + items + "]";
 	}
 	
 }
