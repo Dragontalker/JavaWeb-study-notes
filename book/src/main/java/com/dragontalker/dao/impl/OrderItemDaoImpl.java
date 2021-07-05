44a55970a72cc548b395b83ff6efa8015c15c72d
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:39:23 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/dao/impl/OrderItemDaoImpl.java
+++ book/src/main/java/com/dragontalker/dao/impl/OrderItemDaoImpl.java
@@ -7,8 +7,9 @@ public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
 
 	@Override
 	public int saveOrderItem(OrderItem orderItem) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "insert into t_order_item(`name`, `count`, `price`, `total_price`, `order_id`) values(?, ?, ?, ?, ?)";
+		Object[] args = {orderItem.getName(), orderItem.getCount(), orderItem.getTotalPrice(), orderItem.getOrderId()};
+		return update(sql, args);
 	}
 
 }
