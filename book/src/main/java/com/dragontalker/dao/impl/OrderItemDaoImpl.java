84884c7509606c6ae18d7540f75f9a9e49b7c762
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:37:20 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/dao/impl/OrderItemDaoImpl.java
@@ -0,0 +1,14 @@
+package com.dragontalker.dao.impl;
+
+import com.dragontalker.dao.OrderItemDao;
+import com.dragontalker.pojo.OrderItem;
+
+public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
+
+	@Override
+	public int saveOrderItem(OrderItem orderItem) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
