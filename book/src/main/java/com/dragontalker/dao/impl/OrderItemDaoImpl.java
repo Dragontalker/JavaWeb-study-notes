ff574d24df188a52ab63dba3c12bfb39c585107c
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:55:16 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/dao/impl/OrderItemDaoImpl.java
+++ book/src/main/java/com/dragontalker/dao/impl/OrderItemDaoImpl.java
@@ -8,7 +8,7 @@ public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
 	@Override
 	public int saveOrderItem(OrderItem orderItem) {
 		String sql = "insert into t_order_item(`name`, `count`, `price`, `total_price`, `order_id`) values(?, ?, ?, ?, ?)";
-		Object[] args = {orderItem.getName(), orderItem.getCount(), orderItem.getTotalPrice(), orderItem.getOrderId()};
+		Object[] args = {orderItem.getName(), orderItem.getCount(), orderItem.getPrice(), orderItem.getTotalPrice(), orderItem.getOrderId()};
 		return update(sql, args);
 	}
 
--- book/src/main/java/com/dragontalker/test/OrderItemDaoTest.java
+++ book/src/main/java/com/dragontalker/test/OrderItemDaoTest.java
@@ -1,5 +1,23 @@
 package com.dragontalker.test;
 
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.dragontalker.dao.OrderItemDao;
+import com.dragontalker.dao.impl.OrderItemDaoImpl;
+import com.dragontalker.pojo.OrderItem;
+
 public class OrderItemDaoTest {
 
+	@Test
+	public void testSaveOrderItem() {
+		
+		OrderItemDao orderItemDao = new OrderItemDaoImpl();
+		
+		orderItemDao.saveOrderItem(new OrderItem(null, "Java: 0 to 1", 1, new BigDecimal(100), new BigDecimal(100), "1234567890"));
+		orderItemDao.saveOrderItem(new OrderItem(null, "JavaScript: 0 to 1", 1, new BigDecimal(100), new BigDecimal(100), "1234567890"));
+		orderItemDao.saveOrderItem(new OrderItem(null, "MySQL: 0 to 1", 1, new BigDecimal(100), new BigDecimal(100), "1234567890"));
+
+	}
 }
