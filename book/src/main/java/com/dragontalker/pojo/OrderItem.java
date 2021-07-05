0ed6a6c0c3725c954e0ce0b84d4194e4da10abe6
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:29:21 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/OrderItem.java
+++ book/src/main/java/com/dragontalker/pojo/OrderItem.java
@@ -1,5 +1,13 @@
 package com.dragontalker.pojo;
 
+import java.math.BigDecimal;
+
 public class OrderItem {
 
+	private Integer id;
+	private String name;
+	private Integer count;
+	private BigDecimal price;
+	private BigDecimal totalPrice;
+	private String orderId;
 }
