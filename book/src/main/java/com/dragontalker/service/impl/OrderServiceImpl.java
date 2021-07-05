963d198a7efe52d3c689d9ca9ab6c9cf74013343
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:57:17 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/service/impl/OrderServiceImpl.java
+++ book/src/main/java/com/dragontalker/service/impl/OrderServiceImpl.java
@@ -1,9 +1,16 @@
 package com.dragontalker.service.impl;
 
+import com.dragontalker.dao.OrderDao;
+import com.dragontalker.dao.OrderItemDao;
+import com.dragontalker.dao.impl.OrderDaoImpl;
+import com.dragontalker.dao.impl.OrderItemDaoImpl;
 import com.dragontalker.pojo.Cart;
 import com.dragontalker.service.OrderService;
 
 public class OrderServiceImpl implements OrderService {
+	
+	private OrderDao orderDao = new OrderDaoImpl();
+	private OrderItemDao orderItemDao = new OrderItemDaoImpl();
 
 	@Override
 	public String createOrder(Cart cart, Integer userId) {
