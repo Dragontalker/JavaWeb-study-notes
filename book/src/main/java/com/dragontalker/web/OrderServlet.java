961f15acf7db79db9442fd9d88b87340656d4cd0
Richard <richard.yang.tong@gmail.com>
2021-07-05 18:40:29 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/OrderServlet.java
+++ book/src/main/java/com/dragontalker/web/OrderServlet.java
@@ -6,13 +6,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dragontalker.pojo.Cart;
+import com.dragontalker.pojo.User;
+
 public class OrderServlet extends BaseServlet{
 
 	private static final long serialVersionUID = 4805187894608075566L;
 
 	protected void createOrder(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		
+		// 先获取Cart购物车对象
+		Cart cart = (Cart) req.getSession().getAttribute("cart");
+		
+		// 获取UserId
+		User loginUser = (User) req.getSession().getAttribute("user");
+		
 	}
 
 	
