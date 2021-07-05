7fe60955586d413d729f367a10ace79d4018263e
Richard <richard.yang.tong@gmail.com>
2021-07-05 11:14:27 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/Cart.java
+++ book/src/main/java/com/dragontalker/pojo/Cart.java
@@ -1,5 +1,12 @@
 package com.dragontalker.pojo;
 
-public class Cart {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cart {
+	
+	private Integer totalCount;
+	private BigDecimal totalPrice;
+	private List<CartItem> items = new ArrayList<CartItem>();
 }
