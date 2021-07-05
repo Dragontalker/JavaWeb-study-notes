774bd94855dfdeb687f248b401d09ce9ab6dfd47
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:27:26 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/Order.java
+++ book/src/main/java/com/dragontalker/pojo/Order.java
@@ -8,5 +8,8 @@ public class Order {
 	private String order_id;
 	private Date create_time;
 	private BigDecimal price;
+	// 0未发货, 1已发货, 2已签收
 	private Integer status = 0;
+	private Integer userId;
+	
 }
