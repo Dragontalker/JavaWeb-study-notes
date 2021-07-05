f70b88fc29e4898559429a80b7d60a2925f8cd69
Richard <richard.yang.tong@gmail.com>
2021-07-05 11:12:28 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/CartItem.java
+++ book/src/main/java/com/dragontalker/pojo/CartItem.java
@@ -1,5 +1,12 @@
 package com.dragontalker.pojo;
 
+import java.math.BigDecimal;
+
 public class CartItem {
 
+	private Integer id;
+	private String name;
+	private Integer count;
+	private BigDecimal price;
+	private BigDecimal totalPrice;
 }
