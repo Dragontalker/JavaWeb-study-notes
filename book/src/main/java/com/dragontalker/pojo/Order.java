dc219f09bdd5bc8b7e00bd3c157c7b41cceffb04
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:26:37 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/Order.java
+++ book/src/main/java/com/dragontalker/pojo/Order.java
@@ -1,5 +1,12 @@
 package com.dragontalker.pojo;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Order {
 
+	private String order_id;
+	private Date create_time;
+	private BigDecimal price;
+	private Integer status = 0;
 }
