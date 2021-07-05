7d4e779b8225412cfb7d7d478a07f42d9a315104
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:31:47 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,14 @@
+package com.dragontalker.dao.impl;
+
+import com.dragontalker.dao.OrderDao;
+import com.dragontalker.pojo.Order;
+
+public class OrderDaoImpl implements OrderDao {
+
+	@Override
+	public int saveOrder(Order order) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
