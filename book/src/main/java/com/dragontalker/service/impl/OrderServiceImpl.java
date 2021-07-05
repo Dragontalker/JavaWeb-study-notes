844a79a22a8b46e436c8bcd817dcc92ea81ec4fe
Richard <richard.yang.tong@gmail.com>
2021-07-05 18:07:43 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/service/impl/OrderServiceImpl.java
+++ book/src/main/java/com/dragontalker/service/impl/OrderServiceImpl.java
@@ -1,10 +1,16 @@
 package com.dragontalker.service.impl;
 
+import java.util.Date;
+import java.util.Map;
+
 import com.dragontalker.dao.OrderDao;
 import com.dragontalker.dao.OrderItemDao;
 import com.dragontalker.dao.impl.OrderDaoImpl;
 import com.dragontalker.dao.impl.OrderItemDaoImpl;
 import com.dragontalker.pojo.Cart;
+import com.dragontalker.pojo.CartItem;
+import com.dragontalker.pojo.Order;
+import com.dragontalker.pojo.OrderItem;
 import com.dragontalker.service.OrderService;
 
 public class OrderServiceImpl implements OrderService {
@@ -14,8 +20,22 @@ public class OrderServiceImpl implements OrderService {
 
 	@Override
 	public String createOrder(Cart cart, Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String orderId = System.currentTimeMillis() + "" + userId;
+		
+		Order order = new Order(orderId, new Date(), cart.getTotalPrice(), 0, userId);
+				
+		orderDao.saveOrder(order);
+		
+		for (Map.Entry<Integer, CartItem> entry : cart.getItems().entrySet()) {
+			
+			CartItem cartItem = entry.getValue();
+			
+			OrderItem orderItem = new OrderItem(null, cartItem.getName(), cartItem.getCount(), cartItem.getPrice(), cartItem.getTotalPrice(), orderId);
+			
+			orderItemDao.saveOrderItem(orderItem);
+		}
+	
 	}
 
 }
