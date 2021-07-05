39849240da4f3e93ff2a47ed6eea9f8382b66dc8
Richard <richard.yang.tong@gmail.com>
2021-07-05 11:45:27 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/CartTest.java
+++ book/src/main/java/com/dragontalker/test/CartTest.java
@@ -16,6 +16,8 @@ public class CartTest {
 		cart.addItem(new CartItem(1, "java从入门到精通", 1, new BigDecimal(1000), new BigDecimal(1000)));
 		cart.addItem(new CartItem(2, "数据结构与算法", 1, new BigDecimal(100), new BigDecimal(100)));
 		
+		cart.deleteItem(1);
+		
 		System.out.println(cart);
 	}
 
