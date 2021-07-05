38bc6969d198bdf7270a2b9e811bfa3462b8ef81
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:42:32 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/OrderDaoTest.java
+++ book/src/main/java/com/dragontalker/test/OrderDaoTest.java
@@ -1,5 +1,22 @@
 package com.dragontalker.test;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.dragontalker.dao.OrderDao;
+import com.dragontalker.dao.impl.OrderDaoImpl;
+import com.dragontalker.pojo.Order;
+
 public class OrderDaoTest {
 
+	@Test
+	public void testSaveOrder() {
+		
+		OrderDao orderDao = new OrderDaoImpl();
+		
+		orderDao.saveOrder(new Order("1234567890", new Date(), new BigDecimal(10), 0, 1));
+		
+	}
 }
