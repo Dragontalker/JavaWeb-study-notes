6b028accb2875d71eaf33b82270b612b5cef2474
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:29:49 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/pojo/OrderItem.java
+++ book/src/main/java/com/dragontalker/pojo/OrderItem.java
@@ -23,5 +23,53 @@ public class OrderItem {
 		this.totalPrice = totalPrice;
 		this.orderId = orderId;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public BigDecimal getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(BigDecimal totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
 	
 }
