c26b7e996bb20086b2246129806e4a8b4c547fa1
Richard <richard.yang.tong@gmail.com>
2021-07-05 17:56:26 -0400

    Added notes.

---
+++ book/src/main/java/com/dragontalker/service/impl/OrderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.dragontalker.service.impl;
+
+import com.dragontalker.pojo.Cart;
+import com.dragontalker.service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public String createOrder(Cart cart, Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
