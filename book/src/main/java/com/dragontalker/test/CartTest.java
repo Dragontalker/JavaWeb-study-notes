4905a818843b66241556da22a4582d17b955401e
Richard <richard.yang.tong@gmail.com>
2021-07-05 11:46:36 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/test/CartTest.java
+++ book/src/main/java/com/dragontalker/test/CartTest.java
@@ -15,10 +15,34 @@ public class CartTest {
 		cart.addItem(new CartItem(1, "java从入门到精通", 1, new BigDecimal(1000), new BigDecimal(1000)));
 		cart.addItem(new CartItem(1, "java从入门到精通", 1, new BigDecimal(1000), new BigDecimal(1000)));
 		cart.addItem(new CartItem(2, "数据结构与算法", 1, new BigDecimal(100), new BigDecimal(100)));
+
+		System.out.println(cart);
+	}
+	
+	@Test
+	public void testDeleteItem() {
+		Cart cart = new Cart();
+		cart.addItem(new CartItem(1, "java从入门到精通", 1, new BigDecimal(1000), new BigDecimal(1000)));
+		cart.addItem(new CartItem(1, "java从入门到精通", 1, new BigDecimal(1000), new BigDecimal(1000)));
+		cart.addItem(new CartItem(2, "数据结构与算法", 1, new BigDecimal(100), new BigDecimal(100)));
 		
 		cart.deleteItem(1);
 		
 		System.out.println(cart);
 	}
+	
+	@Test
+	public void clear() {
+		Cart cart = new Cart();
+		cart.addItem(new CartItem(1, "java从入门到精通", 1, new BigDecimal(1000), new BigDecimal(1000)));
+		cart.addItem(new CartItem(1, "java从入门到精通", 1, new BigDecimal(1000), new BigDecimal(1000)));
+		cart.addItem(new CartItem(2, "数据结构与算法", 1, new BigDecimal(100), new BigDecimal(100)));
+		
+		cart.clear();
+		
+		System.out.println(cart);
+	}
+
+
 
 }
