3037ba3945f2c8dc101ac4622510bcc10b498630
Richard <richard.yang.tong@gmail.com>
2021-07-05 18:36:56 -0400

    Added notes.

--- book/src/main/java/com/dragontalker/web/OrderServlet.java
+++ book/src/main/java/com/dragontalker/web/OrderServlet.java
@@ -1,5 +1,19 @@
 package com.dragontalker.web;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class OrderServlet extends BaseServlet{
 
+	private static final long serialVersionUID = 4805187894608075566L;
+
+	protected void createOrder(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	
 }
