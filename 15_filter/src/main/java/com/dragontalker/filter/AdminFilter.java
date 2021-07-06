cebb5f845cd272ada2d4a99997e3dba5fee289a3
Richard <richard.yang.tong@gmail.com>
2021-07-05 20:23:25 -0400

    Added notes.

---
+++ 15_filter/src/main/java/com/dragontalker/filter/AdminFilter.java
@@ -0,0 +1,20 @@
+package com.dragontalker.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class AdminFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
