796b0316141eac140589d6f3fc0bd8ec4d55ebde
Richard <richard.yang.tong@gmail.com>
2021-07-05 20:26:29 -0400

    Added notes.

--- 15_filter/src/main/java/com/dragontalker/filter/AdminFilter.java
+++ 15_filter/src/main/java/com/dragontalker/filter/AdminFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class AdminFilter implements Filter {
 
@@ -16,7 +18,19 @@ public class AdminFilter implements Filter {
 			FilterChain filterChain) 
 					throws IOException, ServletException {
 		
-
+		HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+		
+		HttpSession session = httpServletRequest.getSession();
+		
+		Object user = session.getAttribute("user");
+		
+		if (user == null) {
+			servletRequest.getRequestDispatcher("/login.jsp").forward(servletRequest, servletResponse);
+			return;
+		} else {
+			filterChain.doFilter(httpServletRequest, servletResponse);
+		}
+				
 	}
 
 }
