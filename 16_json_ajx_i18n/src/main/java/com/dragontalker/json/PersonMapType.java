4267e6ebad087bde38b705322eb1b7f86486e86d
Richard <richard.yang.tong@gmail.com>
2021-07-06 14:48:24 -0400

    Added notes.

---
+++ 16_json_ajx_i18n/src/main/java/com/dragontalker/json/PersonMapType.java
@@ -0,0 +1,10 @@
+package com.dragontalker.json;
+
+import java.util.Map;
+
+import com.dragontalker.pojo.Person;
+import com.google.gson.reflect.TypeToken;
+
+public class PersonMapType extends TypeToken<Map<Integer, Person>>{
+
+}
