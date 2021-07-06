748fae9bc7cb31a4539c8d7ea5b5cb82814aa20e
Richard <richard.yang.tong@gmail.com>
2021-07-06 15:07:07 -0400

    Added notes.

--- 16_json_ajx_i18n/src/main/java/com/dragontalker/servlet/AjaxServlet.java
+++ 16_json_ajx_i18n/src/main/java/com/dragontalker/servlet/AjaxServlet.java
@@ -1,5 +1,15 @@
 package com.dragontalker.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class AjaxServlet extends BaseServlet{
 
+    protected void javaScriptAjax(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	
+    	
+    }
 }
