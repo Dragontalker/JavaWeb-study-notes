29022dd7107cbf0c429f86f9493e25415be15904
Richard <richard.yang.tong@gmail.com>
2021-07-05 20:37:33 -0400

    Added notes.

--- 15_filter/src/main/java/com/dragontalker/servlet/LoginServlet.java
+++ 15_filter/src/main/java/com/dragontalker/servlet/LoginServlet.java
@@ -1,7 +1,22 @@
 package com.dragontalker.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -5766922838816592380L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+	
+	
 
 }
