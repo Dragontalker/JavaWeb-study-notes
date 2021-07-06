045c976e50c526ec1410008ebfce6b274b4aa796
Richard <richard.yang.tong@gmail.com>
2021-07-06 14:27:38 -0400

    Added notes.

--- 16_json_ajx_i18n/src/main/java/com/dragontalker/json/JsonTest.java
+++ 16_json_ajx_i18n/src/main/java/com/dragontalker/json/JsonTest.java
@@ -2,11 +2,21 @@ package com.dragontalker.json;
 
 import org.junit.Test;
 
+import com.dragontalker.pojo.Person;
+import com.google.gson.Gson;
+
 public class JsonTest {
 
 	@Test
 	public void test1() {
 		
+		Person person = new Person(1, "GuoGeHaoShuai");
+		
+		Gson gson = new Gson();
+		
+		String personJsonString = gson.toJson(person);
+		
+		System.out.println(personJsonString);
 	}
 	
 }
